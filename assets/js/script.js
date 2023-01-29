70146c9f94e74da83ddd228b748b08f245b2ec98
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-01-28 19:57:16 -0500

    added the geoLocationURL to retrieve lat and lon data points for the city entered by the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var searchInputEl = $("#search-city");
 var searchBtnEl = $("#search-button");
 var cityEl = $("#city");
 var todayDateEl = $("#today-date");
@@ -25,7 +26,7 @@ function display5Days(data) {
     for (var i = 0; i < daysChildrenEl.length; i++) {
         // i would start at index 0 which is the current day. Because the current day is already displayed at the top, we want to ignore it and jump ahead to the next day, that is, the next 8th index (40 hour slots dived by 5 days equal 8 slots per day). 
         // + 1 is added to i to ensure the 5-day forecast always shows the next day
-        var currentEl = data.list[(i*8)+1];
+        var currentEl = data.list[(i * 8) + 1];
         console.log(data);
 
         var futureDays = daysChildrenEl.eq(i).children();
@@ -35,30 +36,44 @@ function display5Days(data) {
         var windFutureEl = futureDays.eq(3);
         var humidityFutureEl = futureDays.eq(4);
 
-        var imageForFuture =  "http://openweathermap.org/img/wn/" + currentEl.weather[0].icon + ".png";
+        var imageForFuture = "http://openweathermap.org/img/wn/" + currentEl.weather[0].icon + ".png";
 
         dateFutureEl.text(dayjs.unix(currentEl.dt).format("dddd"));
         iconFutureEl.html("<img src=" + imageForFuture + ">");
         tempFutureEl.text("Temp: " + currentEl.main.temp + " ℃");
         windFutureEl.text("Wind: " + currentEl.wind.speed + " MPH");
         humidityFutureEl.text("Humidity: " + currentEl.main.humidity + " %")
-        
+
     }
 }
 
 // Function to retrieve weather data from the Open Weather Map API
 // Within this function, we call on the displayToday and display5Days functions as well
 function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=49.2497&lon=-123.1193&units=metric&appid=cccdf7669ae2e9f41bf5e5174cd0a37b';
+    var geoLocationURL = "http://api.openweathermap.org/geo/1.0/direct?appid=cccdf7669ae2e9f41bf5e5174cd0a37b&q=";
+    geoLocationURL = geoLocationURL + searchInputEl.val();
 
-    fetch(requestUrl)
+    fetch(geoLocationURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            displayToday(data);
-            display5Days(data);
-        });
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=cccdf7669ae2e9f41bf5e5174cd0a37b&';
+
+            weatherUrl = weatherUrl + "lat=" + lat + "&lon=" + lon;
+            console.log(weatherUrl);
+
+            fetch(weatherUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    displayToday(data);
+                    display5Days(data);
+                });
+        })
 }
 
 searchBtnEl.click(getApi);
